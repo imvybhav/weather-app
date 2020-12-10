aa53c2c1352c04c33c3dc7c021ef2ef4589bb378
unknown <vybhavkumar03@gmail.com>
2020-12-10 21:24:11 +0530

    forecast data changed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,7 @@ const forecast = (lat, long, callback) => {
             callback('Unable to find, Try another search', undefined)
         }
         else {
-            callback(undefined, body.current.weather_descriptions[0] +', It is currently '+ body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out.')
+            callback(undefined, body.current.weather_descriptions[0] +', It is currently '+ body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out. And the humidity is '+ body.current.humidity +' %')
         }
 
     })
